7aab51af8daefd3c94878d8b4b74fd2b0c4ea184
Salih Çağlar <salihcaglar4187@gmail.com>
2025-05-26 02:33:45 +0300

    Include system.prop

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -9,6 +9,9 @@ TARGET_SPECIFIC_HEADER_PATH += $(LOCAL_PATH)/include
 # Kernel
 TARGET_KERNEL_CONFIG := lineage_trelte_defconfig
 
+# Properties
+TARGET_SYSTEM_PROP += $(LOCAL_PATH)/system.prop
+
 # RIL
 BOARD_MODEM_TYPE := m7450
 
